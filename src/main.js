17fa46bd962435b3df41bb943d985a6f63ac2e24
VincenzoKhoa <khoa.vm.512@gmail.com>
2022-06-27 15:03:14 +0700

    first

---
+++ src/main.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const { extname } = require('path');
+const { log } = require('console');
+const app = express();
+const port = 3000;
+
+const route = require('./routes')
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+	extended: true
+}))
+
+app.use(express.json())
+// HTTP logger
+// app.use(morgan('combined'));
+
+
+//Template engine
+app.engine('hbs', handlebars.engine({
+	extname: '.hbs'
+}
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log(path.join(__dirname, 'views'));
+
+
+
+route(app);
+
+
+app.listen(port, () => {
+	console.log(`app listening ${port}`)
+})	
